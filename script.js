48d9dac3afdd5f1d0e668911355fa3f4f4a19213
Lindsey Wilhite <thetabloom1@gmail.com>
2018-08-25 20:41:25 -0600

    Clear input function and disable enter button function

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ var deleteButton = document.querySelector('.delete-button');
 
 
 enterButton.addEventListener('click', addWebsite);
+titleInputField.addEventListener('keyup', enableButton);
+urlInputField.addEventListener('keyup', enableButton);
+
 
 
 function addWebsite() {
@@ -30,7 +33,22 @@ function addWebsite() {
             </li>`
         var listWebsites = document.querySelector("ul");
         listWebsites.appendChild(newBookmark);
+        clearInputFields();
+        enableButton();
 };
+
+function clearInputFields(){
+  titleInputField.value = '';
+  urlInputField.value = '';
+}
+
+function enableButton(){
+  if (titleInputField.value === '' || urlInputField.value === '') {
+    enterButton.disabled = true;
+  } else {
+    enterButton.disabled = false;
+  }
+}
 
 
 
@@ -44,3 +62,23 @@ function addWebsite() {
 // *A class of .read should be added to the bookmark
 // *If it already has the class of .read, it should be removed
 // *When the user clicks on the “Delete” button, the link should be removed from the page
+
+// Phase Two
+// If the user omits the title or the URL, the application should not create the link and should instead display an error.
+// The application should be responsive and work equally well on desktop and mobile.
+
+// Phase Three
+// The button for creating links should be disabled if there are no contents in the title or URL fields.
+// The application should be able to keep count of the total number of links currently on the page.
+// The application should be able to keep count of the total number of read and unread links currently on the page.
+
+// Phase Four: The Project Strikes Back
+// Add a “Clear Read Bookmarks” button which clears all the read bookmarks when clicked.
+// The user should not to be able to add a URL that isn’t valid.
+
+
+
+
+
+
+
